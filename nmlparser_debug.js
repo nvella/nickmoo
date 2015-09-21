3c3bb0e0eb9b42900fc2c17fe3ae0455f6746b76
Nick Vella <nick@nxk.me>
2015-09-21 17:29:30 +1000

    nml parser debug repl script

---
+++ nmlparser_debug.js
@@ -0,0 +1,36 @@
+var readline = require('readline');
+var P = require('./lib/nml/parser');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var util = require('util');
+
+var buffer = '';
+
+function p(obj) {
+  console.log(util.inspect(obj, {depth: 16, colors: true}));
+}
+
+function runCode(code) {
+  console.log("Running code:\n"+code+"\n------");
+  try {
+    p(P.codeToAst(code));
+  } catch(err) {
+    console.log('Syntax error\n'+err);
+  }
+}
+
+function doPrompt() {
+  rl.question('>', function(input) {
+    if(input.trim() == '.run') {
+      runCode(buffer);
+      buffer = '';
+    } else {
+      buffer += input + "\n";
+    }
+    doPrompt();
+  });
+}
+
+doPrompt();
