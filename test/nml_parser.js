6ae59e03cf1cbcb1616f01c2e9fa4d74b24f834a
Nick Vella <nick@nxk.me>
2015-09-23 13:57:31 +1000

    tests for NML.Parser.parseLine

--- test/nml_parser.js
+++ test/nml_parser.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var Parser = require('../lib/nml/parser');
+var ObjectId = require('mongodb').ObjectId;
 
 describe('NML.Parser', function() {
   describe('parseLine', function() {
@@ -21,5 +22,56 @@ describe('NML.Parser', function() {
       expect(Parser.parseLine("'the quick 4 brown fox'")).to.eql([
         'the quick 4 brown fox']);
     });
+
+    it('handles local variables', function() {
+      expect(Parser.parseLine('abc $myVar def')).to.eql(['abc', {type: 'var',
+        name: 'myVar'}, 'def']);
+    });
+
+    it('handles object properties', function() {
+      expect(Parser.parseLine('abc %myProp def')).to.eql(['abc', {type: 'prop',
+        name: 'myProp'}, 'def']);
+    });
+
+    it('handles comments', function() {
+      expect(Parser.parseLine('the quick brown ;fox jumped over $lazy %dog (1 2 3 4)')).
+        to.eql(['the', 'quick', 'brown', {type: 'comment', text:
+        'fox jumped over $lazy %dog (1 2 3 4)'}]);
+    });
+
+    it('handles object ids', function() {
+      expect(Parser.parseLine('the quick brown #012345678901234567890123')).to.
+        eql(['the', 'quick', 'brown', new ObjectId('012345678901234567890123')]);
+    });
+
+    it('handles object id aliases', function() {
+      expect(Parser.parseLine('the quick ##root')).to.eql(['the', 'quick',
+        {type: 'objectIdAlias', value: 'root'}]);
+    });
+
+    it('handles arrays', function() {
+      expect(Parser.parseLine('hey [1 2 3 4] there')).to.eql(['hey',
+        {type: 'array', ctx: [1, 2, 3, 4]}, 'there']);
+    });
+
+    it('handles indexes on indexable components', function() {
+      expect(Parser.parseLine('hey $myVar[24] there')).to.eql(['hey',
+        {type: 'var', name: 'myVar', ctx: [24]}, 'there']);
+
+      expect(Parser.parseLine('hey %myProp[32] there')).to.eql(['hey',
+        {type: 'prop', name: 'myProp', ctx: [32]}, 'there']);
+    });
+
+    it('groups components in parens', function() {
+      expect(Parser.parseLine('this is (a very nice test) 1 2')).to.eql([
+        'this',
+        'is',
+        {type: 'group', ctx: ['a', 'very', 'nice', 'test']},
+        1,
+        2
+      ]);
+    });
   });
+
+
 });
