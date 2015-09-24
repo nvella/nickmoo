e712e62c37cc9dcc82c17914cbb140a0b9e0d3c3
Nick Vella <nick@nxk.me>
2015-09-25 00:14:02 +1000

    tests for MObject#load

--- test/mobject.js
+++ test/mobject.js
@@ -2,6 +2,22 @@ var expect = require('chai').expect;
 var MObject = require('../lib/mobject');
 var ObjectId = require('mongodb').ObjectId;
 
+function FakeMongoCollection(spec) {
+  this.spec = spec;
+}
+
+// only finds by id
+FakeMongoCollection.prototype.findOne = function(queryDoc, callback) {
+  for(var obj of this.spec) {
+    if(obj._id == queryDoc._id) return callback(null, [obj]);
+  }
+  callback(null, []);
+};
+
+FakeMongoCollection.prototype.updateOne = function(queryDoc, doc, options, callback) {
+  // TODO
+};
+
 describe('MObject', function() {
   describe('new', function() {
     it('creates a new object id for itself when one isn\'t provided',
@@ -16,6 +32,55 @@ describe('MObject', function() {
       var now = new Date() / 1000;
       expect(mobj.created).to.be.gte(now - 1);
       expect(mobj.created).to.be.lte(now + 1);
+    });
+  });
+
+  describe('#load', function() {
+    var app = {
+      collections: {
+        objects: new FakeMongoCollection(
+          [{
+            _verbs: {
+              _step: '$a = 1'
+            },
+            _created: 12345,
+            ayy: 'lmao'
+          }]
+        )
+      }
+    };
+
+    it('can load it\'s state from a mongodb document', function(done) {
+      var mobj = new MObject(app);
+      app.collections.objects.spec[0]._id = mobj.id;
+
+      mobj.load(function(err) {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+
+    it('can load properties', function(done) {
+      var mobj = new MObject(app);
+      app.collections.objects.spec[0]._id = mobj.id;
+
+      mobj.load(function(err) {
+        expect(err).to.be.null;
+        expect(mobj.props.ayy).to.equal('lmao');
+        done();
+      });
+    });
+
+    it('can load verbs', function(done) {
+      var mobj = new MObject(app);
+      app.collections.objects.spec[0]._id = mobj.id;
+
+      mobj.load(function(err) {
+        expect(err).to.be.null;
+        expect(mobj.verbs._step).to.not.be.undefined;
+        expect(mobj.verbs._step.src).to.equal('$a = 1');
+        done();
+      });
     });
   });
 });
