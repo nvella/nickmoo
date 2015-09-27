1ae0689b12b4a3b2a13cf81b9951cc03db5d2e1e
Nick Vella <nick@nxk.me>
2015-09-27 17:53:56 +1000

    rename Menus to UI

--- test/menus_login.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect;
-var Login = require('../lib/menus/login');
-
-describe('Menus.Login', function() {
-  describe('#init', function() {
-
-  });
-});
---
+++ test/ui_login.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var Login = require('../lib/menus/login');
+var Connection = require('../lib/connection');
+var FakeSock = require('./lib/fake_sock');
+var FakeApp = require('./lib/fake_app');
+
+describe('UI.Login', function() {
+  describe('#init', function() {
+
+  });
+});
