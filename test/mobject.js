2ad11405c0ac576ac316a64afa6ec8f1d3f1ac87
Nick Vella <nick@nxk.me>
2015-09-29 15:22:19 +1000

    MObject: start redesign to a simple DB interface (MObj is just an interface to the db, it stores no state)

--- test/mobject.js
+++ test/mobject.js
@@ -31,6 +31,11 @@ FakeMongoCollection.prototype.updateOne = function(queryDoc, doc, options, callb
   cb(new Error('no docs match'));
 };
 
+FakeMongoCollection.prototype.insertOne = function(doc, callback) {
+  this.spec.push(doc);
+  callback();
+};
+
 describe('MObject', function() {
   describe('constructor', function() {
     it('creates a new object id for itself when one isn\'t provided',
@@ -38,75 +43,9 @@ describe('MObject', function() {
       var mobj = new MObject();
       expect(mobj.id).to.be.an.instanceof(ObjectId);
     });
-
-    it('sets the creation time to within a second of the object being created',
-      function() {
-      var mobj = new MObject();
-      var now = new Date() / 1000;
-      expect(mobj.created).to.be.gte(now - 1);
-      expect(mobj.created).to.be.lte(now + 1);
-    });
   });
 
-  describe('#load', function() {
-    var app = {
-      collections: {
-        objects: new FakeMongoCollection(
-          [{
-            _verbs: {
-              _step: '$a = 1'
-            },
-            _created: 12345,
-            ayy: 'lmao'
-          }]
-        )
-      }
-    };
 
-    it('can load it\'s state from a mongodb document', function(done) {
-      var mobj = new MObject(app);
-      app.collections.objects.spec[0]._id = mobj.id;
-
-      mobj.load(function(err) {
-        expect(err).to.be.null;
-        done();
-      });
-    });
-
-    it('can load properties', function(done) {
-      var mobj = new MObject(app);
-      app.collections.objects.spec[0]._id = mobj.id;
-
-      mobj.load(function(err) {
-        expect(err).to.be.null;
-        expect(mobj.props.ayy).to.equal('lmao');
-        done();
-      });
-    });
-
-    it('can load verbs', function(done) {
-      var mobj = new MObject(app);
-      app.collections.objects.spec[0]._id = mobj.id;
-
-      mobj.load(function(err) {
-        expect(err).to.be.null;
-        expect(mobj.verbs._step).to.not.be.undefined;
-        expect(mobj.verbs._step.src).to.equal('$a = 1');
-        done();
-      });
-    });
-
-    it('can load the creation time', function(done) {
-      var mobj = new MObject(app);
-      app.collections.objects.spec[0]._id = mobj.id;
-
-      mobj.load(function(err) {
-        expect(err).to.be.null;
-        expect(mobj.created).to.equal(12345);
-        done();
-      });
-    });
-  });
 
   describe('#getProp', function() {
     var app = {
@@ -134,13 +73,13 @@ describe('MObject', function() {
       });
     });
 
-    it('returns NML null on any property prefixed with an _', function(done) {
+    it('returns NML null on a no-read property', function(done) {
       var mobj = new MObject(app);
       app.collections.objects.spec[0]._id = mobj.id;
 
       mobj.load(function(err) {
         expect(err).to.be.null;
-        expect(mobj.getProp('_created')).to.eql({type: 'null', value: null});
+        expect(mobj.getProp('_verbs')).to.eql({type: 'null', value: null});
         done();
       });
     });
@@ -154,8 +93,7 @@ describe('MObject', function() {
             _verbs: {
               _step: '$a = 1'
             },
-            _created: 12345,
-            ayy: 'lmao'
+            _created: 12345
           }]
         )
       }
@@ -165,22 +103,20 @@ describe('MObject', function() {
       var mobj = new MObject(app);
       app.collections.objects.spec[0]._id = mobj.id;
 
-      mobj.load(function(err) {
+      mobj.setProp('ayy', 'memes', function(err) {
         expect(err).to.be.null;
-        mobj.setProp('ayy', 'memes');
-        expect(mobj.getProp('ayy')).to.equal('memes');
+        expect(app.collections.objects.spec[0].ayy).to.equal('memes');
         done();
       });
     });
 
-    it('doesn\'t set props prefixed with _', function(done) {
+    it('doesn\'t set no-read props', function(done) {
       var mobj = new MObject(app);
       app.collections.objects.spec[0]._id = mobj.id;
 
-      mobj.load(function(err) {
+      mobj.setProp('_created', 'ayy', function(err) {
         expect(err).to.be.null;
-        mobj.setProp('_test', 'test');
-        expect(mobj.props._test).to.be.undefined;
+        expect(app.collections.objects.spec[0]._created).to.equal(12345);
         done();
       });
     });
