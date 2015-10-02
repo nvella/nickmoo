ba5d8167cf2b67c30602124b0fe98e3af8a31dff
Nick Vella <nick@nxk.me>
2015-10-02 14:27:18 +1000

    util: Basic db-init tool

---
+++ util/db-init-welcomemsg.js
@@ -0,0 +1,21 @@
+module.exports = "\n"+
+"       _      _\n"+
+" _ __ (_) ___| | ___ __ ___   ___   ___\n"+
+"| '_ \\| |/ __| |/ / '_ ` _ \\ / _ \\ / _ \\   {SERVER_NAME}\n"+
+"| | | | | (__|   <| | | | | | (_) | (_) |  running {NM_NAME} version {NM_VERSION}\n"+
+"|_| |_|_|\\___|_|\\_\\_| |_| |_|\\___/ \\___/\n"+
+"\n"+
+"+= WHAT? =====================================+= GET STARTED ==============-+\n"+
+"|                                             |                             |\n"+
+"|   NickMOO is an online, multiplayer,        |  Type                       |\n"+
+"|  reprogrammable and user content driven     |   guest                     |\n"+
+"|  text game, historically known as a MOO.    |           to play as guest  |\n"+
+"|   Most of the world is created by the       |   connect <user> <pw>       |\n"+
+"|  players themselves, with new areas and     |                   to login  |\n"+
+"|  objects being created in the game every    |   new                       |\n"+
+"|  day.                                       |       to create a new user  |\n"+
+"|   For more info, join as a guest and        |              on the system  |\n"+
+"|  'ask the Guide for help'                   |                             |\n"+
+"|                                             |                             |\n"+
+"+---------------------------------------------+-----------------------------+\n"+
+"\n";
