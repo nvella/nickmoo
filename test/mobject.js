3976a562fb6564fff8c257a4f50726df165abc3b
Nick Vella <nick@nxk.me>
2015-10-04 22:56:09 +1100

    test: MObject: #verbcall: start of tests

--- test/mobject.js
+++ test/mobject.js
@@ -563,4 +563,52 @@ describe('MObject', function() {
       });
     })
   });
+
+  describe('#verbcall', function() {
+    it('can create a vm from a local verbcall', function(done) {
+      var dirObj = new ObjectId();
+      var indirObj = new ObjectId();
+      var verbcall = {
+        type: 'verbcall',
+        verb: 'put',
+        directObj: dirObj,
+        prepos: 'in',
+        indirectObj: indirObj,
+        params: [dirObj, 'in', indirObj]
+      };
+
+      mobj.verbcall(verbcall, function(err, vm) {
+        expect(err).to.be.null;
+        expect(vm).to.be.an.instanceof(NML.VM);
+        expect(vm.state.ast).to.eql([{type: 'verb', src: '$a = 2'}]);
+        expect(vm.state.localVars).to.eql({
+          _verb: 'put',
+          _directObj: dirObj,
+          _prepos: 'in',
+          _indirectObj: indirObj,
+          _params: [dirObj, 'in', indirObj]
+        });
+        done();
+      });
+    });
+
+    it('returns an error when the verb doesn\'t exist', function(done) {
+      var dirObj = new ObjectId();
+      var indirObj = new ObjectId();
+      var verbcall = {
+        type: 'verbcall',
+        verb: 'noExist',
+        directObj: dirObj,
+        prepos: 'in',
+        indirectObj: indirObj,
+        params: [dirObj, 'in', indirObj]
+      };
+
+      mobj.verbcall(verbcall, function(err, vm) {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.message).to.equal('verb does not exist');
+        done();
+      });
+    });
+  });
 });
